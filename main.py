5bb6b95e47a9a7d834e5371b71ce7fe1d2b13dad
huy-vo <nhathuyhoangvo@gmail.com>
2024-12-29 14:13:43 -0500

    up

--- main.py
+++ main.py
@@ -3,23 +3,48 @@ from hand import *
 from settings import *
 import ctypes, platform, pygame, sys
 
-if platform.system() == 'Window':
+if platform.system() == 'Windows':
     ctypes.windll.user32.SetProcessDPIAware()
 
 class Game:
     def __init__(self):
-        
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
-        pygame.display.set_caption(TITLE)
+        pygame.display.set_caption(TITLE_STRING)
         self.clock = pygame.time.Clock()
         self.hand = Hand()
+
 
 
     def run(self):
 
         self.start_time = pygame.time.get_ticks()
         while True:
-            for event in pygame.event.get()
-            
+            for event in pygame.event.get():
+                if event.type ==  pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if event.button == 1:
+                        mouse_down = True
+
+                if event.type == pygame.MOUSEBUTTONUP:
+                    if event.button == 1:
+                        if mouse_down:
+                            mouse_down = False
+                            self.hand = Hand()
+
+
+            self.delta_time = (pygame.time.get_ticks() - self.start_time) / 1000
+            self.start_time = pygame.time.get_ticks()
+            pygame.display.update()
+            self.screen.fill(BG_COLOR)
+
+            self.hand.update()
+            self.clock.tick(FPS)
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
 
